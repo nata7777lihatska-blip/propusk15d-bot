0f4cb5287a36f3d64bc2417ce1fbda98221a6dec
nata7777lihatska-blip <nata7777lihatska@gmail.com>
2025-11-13 23:07:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,6 @@ def webhook():
 # ========= Запуск бота + Flask =========
 
 if __name__ == "__main__":
-    import threading
-    threading.Thread(target=tg_app.run_polling, daemon=True).start()
-
+    
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
